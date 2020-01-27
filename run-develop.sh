04d7ee3be55968c9951439513aad9b751d9a7176
Antonio D'Alessio <antonio.dalessio@satispay.com>
2020-01-27 21:33:32 +0100

    Check if documentation path exist

--- run-develop.sh
+++ run-develop.sh
@@ -4,6 +4,11 @@ then
       echo "\$GATSBY_DOCUMENTATION_PATH is empty"
       exit 1
 fi
+
+if [ ! -d "$GATSBY_DOCUMENTATION_PATH" ]; then
+    echo "$GATSBY_DOCUMENTATION_PATH must exist"
+    exit 1
+fi
 docker run -d -t -i --rm \
 --name "gatsby_documentation" \
 -v $GATSBY_DOCUMENTATION_PATH:/app/webapp/content \
